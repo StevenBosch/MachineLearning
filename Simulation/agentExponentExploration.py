Fixed the normalization, I think
Also added names for the papers

Simulation/agentExponentExploration.py
@@ -58,14 +58,16 @@ class Agent:
             self.action = Actions["grab"]
         else:
             sample = np.random.random_sample()
+            smean = np.mean(np.exp(self.q[self.state[0], self.state[1], :,
+                               self.grasped]/tau))
             single = np.exp(self.q[self.state[0], self.state[1], :,
-                            self.grasped]/tau)
+                            self.grasped]/tau)/smean
             if any(single == float('inf')):
                 print("tau ", tau)
                 print("q values ", self.q[self.state[0], self.state[1], :,
                                           self.grasped])
             total = sum(np.exp(self.q[self.state[0], self.state[1], :,
-                               self.grasped]/tau))
+                               self.grasped]/tau)/smean)
             probs = single / total
 
             for action in Actions2:

Simulation/main.py
@@ -1,6 +1,6 @@
 """Our main module."""
 import world as w
-import agent as ag
+import agentExponentExploration as ag
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -160,7 +160,7 @@ if __name__ == "__main__":
     new_world.add_objects()
     new_world.print_map()
 
-    epochs = 500
+    epochs = 2000
     steps = np.zeros((2,epochs))
     for epoch in range(epochs):
         print(epoch)