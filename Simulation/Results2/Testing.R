865faaca6fc782a7fa94ee717e32f07eb317ebb7
StevenBosch <stevenbosch@live.nl>
2016-02-04 21:25:24 +0100

    finished results

--- Simulation/Results2/Testing.R
+++ Simulation/Results2/Testing.R
@@ -1,17 +1,38 @@
-single = read.csv("ConvergenceSingle.csv", sep="\t")
-team = read.csv("ConvergenceTeam.csv", sep="\t")
+### ------- Convergence -------- ###
+
+single = read.csv("Convergence_single.csv", sep="\t")
+team = read.csv("Convergence_team.csv", sep="\t")
 
 t.test(single$Not_grabbed, team$Not_grabbed)
-t.test(single$Grabbed-single$Not_grabbed, team$Grabbed-team$Not_grabbed)
 t.test(single$Grabbed, team$Grabbed)
+t.test(single$Grabbed + single$Not_grabbed, team$Grabbed + team$Not_grabbed)
+
+#par(mfrow=c(1,2))
+
+plot(single$Not_grabbed, ylim = c(0,3000), col=150, pch=16, main = "Epochs before convergence", ylab = "Epoch", xlab = "Run")
+points(team$Not_grabbed, col=51, pch=17)
+legend("bottomright", c("single", "team"), col=c(150, 51),pch=c(16, 17))
+
+plot(single$Grabbed, col=150, ylim = c(0,3000), pch=16, main = "Epochs before convergence", ylab = "Epoch", xlab = "Run")
+points(team$Grabbed, col=51, pch=17)
+legend("topright", c("single", "team"), col=c(150, 51),pch=c(16, 17))
 
 
-par(mfrow=c(1,2))
+### ------- Path -------- ###
 
-plot(single$Not_grabbed, ylim = c(0,3000), col="blue", pch=16, main = "Epochs before convergence (not grabbed)")
-points(team$Not_grabbed, col="red", pch=17)
-legend("bottomright", c("single", "team"), col=c("blue", "red"),pch=c(16, 17))
+single = read.csv("Path_single.csv", sep="\t")
+team = read.csv("Path_team.csv", sep="\t")
 
-plot(single$Grabbed, col="blue", ylim = c(0,3000), pch=16, main = "Epochs before convergence (grabbed)")
-points(team$Grabbed, col="red", pch=17)
-legend("topright", c("single", "team"), col=c("blue", "red"),pch=c(16, 17))
+t.test(single$Not_grabbed, team$Not_grabbed)
+t.test(single$Grabbed, team$Grabbed)
+t.test(single$Grabbed + single$Not_grabbed, team$Grabbed + team$Not_grabbed)
+
+#par(mfrow=c(1,2))
+
+plot(single$Not_grabbed, ylim = c(0,60), col=150, pch=16, main = "Steps of final path", ylab = "Epoch", xlab = "Run")
+points(team$Not_grabbed, col=51, pch=17)
+legend("bottomright", c("single", "team"), col=c(150, 51),pch=c(16, 17))
+
+plot(single$Grabbed, col=150, ylim = c(0,20), pch=16, main = "Steps of final path", ylab = "Epoch", xlab = "Run")
+points(team$Grabbed, col=51, pch=17)
+legend("topright", c("single", "team"), col=c(150, 51),pch=c(16, 17))
--- Simulation/Results_3000epochs_30runs/Testing.R
+++ Simulation/Results_3000epochs_30runs/Testing.R
@@ -4,12 +4,12 @@ team = read.csv("ConvergenceTeam.csv", sep="\t")
 t.test(single$Not_grabbed, team$Not_grabbed)
 t.test(single$Grabbed, team$Grabbed)
 
-par(mfrow=c(1,2))
+#par(mfrow=c(1,2))
 
-plot(single$Not_grabbed, ylim = c(0,3000), col="blue", pch=16, main = "Epochs before convergence (not grabbed)")
+plot(single$Not_grabbed, ylim = c(0,3000), col="blue", pch=16, main = "Epochs before convergence (not grabbed)", ylab = "Epoch", xlab = "Run")
 points(team$Not_grabbed, col="red", pch=17)
 legend("bottomright", c("single", "team"), col=c("blue", "red"),pch=c(16, 17))
 
-plot(single$Grabbed, col="blue", ylim = c(0,3000), pch=16, main = "Epochs before convergence (grabbed)")
+plot(single$Grabbed, col="blue", ylim = c(0,3000), pch=16, main = "Epochs before convergence (grabbed)", ylab = "Epoch", xlab = "Run")
 points(team$Grabbed, col="red", pch=17)
 legend("topright", c("single", "team"), col=c("blue", "red"),pch=c(16, 17))
