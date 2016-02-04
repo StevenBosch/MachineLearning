f0a5ef0cbb692ac45c971f39caee86aba856f724
StevenBosch <stevenbosch@live.nl>
2016-02-04 17:40:29 +0100

    results2

---
+++ Simulation/Results2/Testing.R
@@ -0,0 +1,17 @@
+single = read.csv("ConvergenceSingle.csv", sep="\t")
+team = read.csv("ConvergenceTeam.csv", sep="\t")
+
+t.test(single$Not_grabbed, team$Not_grabbed)
+t.test(single$Grabbed-single$Not_grabbed, team$Grabbed-team$Not_grabbed)
+t.test(single$Grabbed, team$Grabbed)
+
+
+par(mfrow=c(1,2))
+
+plot(single$Not_grabbed, ylim = c(0,3000), col="blue", pch=16, main = "Epochs before convergence (not grabbed)")
+points(team$Not_grabbed, col="red", pch=17)
+legend("bottomright", c("single", "team"), col=c("blue", "red"),pch=c(16, 17))
+
+plot(single$Grabbed, col="blue", ylim = c(0,3000), pch=16, main = "Epochs before convergence (grabbed)")
+points(team$Grabbed, col="red", pch=17)
+legend("topright", c("single", "team"), col=c("blue", "red"),pch=c(16, 17))
